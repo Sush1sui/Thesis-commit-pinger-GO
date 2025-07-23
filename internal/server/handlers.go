e1043243c5e29edfb5f240046f0e7ca009fea4da
Sush1sui <jplovesraw@gmail.com>
2025-07-23 19:19:22 +0800

    refactor: added another branch

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -96,7 +96,7 @@ func SendNotification(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Received webhook payload:", payload)
 
-	if payload.Ref == "refs/heads/main" || payload.Ref == "refs/heads/master" || payload.Ref == "refs/heads/frontend" {
+	if payload.Ref == "refs/heads/main" || payload.Ref == "refs/heads/master" || payload.Ref == "refs/heads/frontend" || payload.Ref == "refs/heads/frontend-only" {
 		commit := payload.HeadCommit
 		ogImage, err := getOpenGraphImage(commit.URL)
 		if err != nil {
