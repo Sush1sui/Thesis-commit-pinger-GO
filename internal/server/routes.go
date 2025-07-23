6076f048c97c0adb4d541ed3f6862dc5e4a7d202
Sush1sui <jplovesraw@gmail.com>
2025-07-23 13:56:32 +0800

    refactor: forgot to add route

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -8,6 +8,8 @@ func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", IndexHandler)
+	// post request handler for GitHub webhook
+	mux.HandleFunc("/github-webhook", SendNotification)
 
 	return mux
 }
